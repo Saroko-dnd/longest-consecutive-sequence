535de112e9ab5c07aa4c7e575400e8f5ace3b0c2
saroko-dnd <isaroko@mail.ru>
2018-02-26 21:23:56 +0200

    function longestConsecutiveLength was successfully implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+    let allIntegers;
+    let maxLengthOfSequence = 0;
+    let bufferForLengthOfSequence = 0;
+
+    allIntegers = array.reduce((storageObject, number) => {
+        storageObject[number] = number;
+
+        if (storageObject.min === undefined) {
+            storageObject.min = number;
+        } else if (storageObject.min > number) {
+            storageObject.min = number;
+        }
+
+        if (storageObject.max === undefined) {
+            storageObject.max = number;
+        } else if (storageObject.max < number) {
+            storageObject.max = number;
+        }
+
+        return storageObject;
+    }, {});
+
+    for (let number = allIntegers.min; number <= allIntegers.max; ++number) {
+        if (!bufferForLengthOfSequence && allIntegers[number] !== undefined) {
+            bufferForLengthOfSequence = 1;
+        } else if (
+            allIntegers[number] !== undefined &&
+            allIntegers[number - 1] !== undefined
+        ) {
+            ++bufferForLengthOfSequence;
+        } else {
+            if (maxLengthOfSequence < bufferForLengthOfSequence) {
+                maxLengthOfSequence = bufferForLengthOfSequence;
+            }
+            bufferForLengthOfSequence = 0;
+        }
+    }
+
+    return maxLengthOfSequence > bufferForLengthOfSequence
+        ? maxLengthOfSequence
+        : bufferForLengthOfSequence;
+};
